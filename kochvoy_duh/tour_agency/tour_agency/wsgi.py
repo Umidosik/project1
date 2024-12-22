101a758eb43193e0ad48da1d12df1c5b8e0b76fc
Umida <umidaraimova3@gmail.com>
2024-12-22 22:20:00 +0600

    Update wsgi.py
    qwertyuio

--- kochvoy_duh/tour_agency/tour_agency/wsgi.py
+++ kochvoy_duh/tour_agency/tour_agency/wsgi.py
@@ -7,6 +7,19 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
+
+
+
+
+
+
+
+
+
+
+
+
+
 import os
 
 from django.core.wsgi import get_wsgi_application
